97787e514ba15700588b14df00489eb73608fc58
Kevin Linuhung <kevinlinuhung@gmail.com>
2024-07-29 20:13:50 +0700

    [ADD] - Custom Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                roboto: ["Roboto"],
+            },
+        },
+    },
+    plugins: [],
+};
